b41cbcea3492019e8786c08be85247b039ac711b
titissamp <titissamp@gmail.com>
2021-03-15 11:24:06 +0700

    Commission
    Membuat class Commission sampai instruksi 4

---
+++ W1/Polymorphism/src/polymorphism/Commission.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymorphism;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Commission extends Hourly {
+    private double TotalSales;
+    private double CommissionRate;
+    
+    public Commission (String eName, String eAddress, String ePhone, String socSecNumber, double rate, double Comrate)
+    {
+        super (eName, eAddress, ePhone, socSecNumber, rate);
+        Comrate = CommissionRate;
+    }
+    public void addSales (double Total){
+        TotalSales = Total + TotalSales;
+    }
+    
+}
---
+++ W1/Polymorphism/src/polymorphism/Firm.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymorphism;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Firm {
+//-----------------------------------------------------------------
+// Creates a staff of employees for a firm and pays them.
+//-----------------------------------------------------------------
+public static void main (String[] args)
+{
+    Staff personnel = new Staff();
+    personnel.payday();
+    }
+}
+    
