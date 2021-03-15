b422acac8d3a1254aa2b91da1f607f3cba684b8e
titissamp <titissamp@gmail.com>
2021-03-15 12:19:44 +0700

    Painting Shapes
    Menambahkan class Rectangle dan Cylinder, dan juga menambahkan konstruktor pada class Shapes

---
+++ W1/PolymorphismSorting/src/polymorphismsorting/Cylinder.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymorphismsorting;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Cylinder extends Shapes {
+    private double radius; 
+    private double height;
+    //radius in feet
+    //----------------------------------
+    // Constructor: Sets up the sphere.
+    //----------------------------------
+    public Cylinder(double r, double h)
+    {
+        super("Cylinder");
+        radius = r;
+        height = h;
+    }
+//-----------------------------------------
+// Returns the surface area of the sphere.
+//-----------------------------------------
+    public double area()
+    {
+        return Math.PI*radius*radius*height;
+    }
+//-----------------------------------
+// Returns the sphere as a String.
+//-----------------------------------
+    public String toString()
+    {
+        return super.toString() + " of radius " + radius + " of height " + height;
+    }
+}
