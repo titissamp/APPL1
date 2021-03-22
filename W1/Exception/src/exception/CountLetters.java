8b03299d8f05e66ad11d6db1cb5cb3944fa2cfd0
titissamp <titissamp@gmail.com>
2021-03-22 11:04:04 +0700

    Exception Aren't Always Errors
    fix

--- W1/Exception/src/exception/CountLetters.java
+++ W1/Exception/src/exception/CountLetters.java
@@ -9,13 +9,29 @@ package exception;
  *
  * @author LENOVO
  */
-public class CountLetters {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+import java.util.Scanner;  
+public class CountLetters  
+{  
+ public static void main(String[] args)  
+ {  
+    int[] counts = new int[26];  
+    Scanner scan = new Scanner(System.in);  
+ //get word from user  
+    System.out.print("Enter a single word (letters only, please): ");   
+    String word = scan.nextLine();  
+ //convert to all upper case  
+    word = word.toUpperCase();  
+ //count frequency of each letter in string 
+    try{
+    for (int i=0; i < word.length(); i++)  
+        counts[word.charAt(i)-'A']++;  
+        //print frequencies  
+    System.out.println();  
+    for (int i=0; i < counts.length; i++)  
+        if (counts [i] != 0)  
+            System.out.println((char)(i +'A') + ": " + counts[i]);   
+    } catch (Exception e){
+        System.out.println("Not a letter");
+ }      
+} 
 }
